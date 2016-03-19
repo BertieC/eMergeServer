2771dc8c671a87f2a0308f2df4d9c97b43d31099
bertie <coetzeealbertus@gmail.com>
2016-03-20 01:23:15 +0200

    addUser Functionallity added

--- eMerge.js
+++ eMerge.js
@@ -6,9 +6,29 @@ var port = process.env.PORT || 8080;
 var http = require('http');
 var express = require("express");
 var app = express();
+var Firebase = require("firebase");
+var myFirebaseRef = new Firebase("https://torrid-fire-226.firebaseio.com");
 
+//just an test call for debugging
   app.get("/test", function(req, res){
     res.send("Test works");
+  });
+
+//Adds new users to the database "Registers them"
+  app.get("/addUsr", function(req, res){
+    var fname = req.param("fname");
+    var lname = req.param("lname");
+    var town = req.param("town");
+    var phone = req.param("phone");
+    var usrRef = myFirebaseRef.child("citizens").child(phone);
+
+    usrRef.set(
+      {
+      "fname":fname,
+      "lname":lname,
+      "town":town
+    });
+    console.log("added new User...");
   });
 
 var server = app.listen(port, function(){
