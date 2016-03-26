1249fac9d4e645385e66945cc1e65c4298c58fb0
bertie <coetzeealbertus@gmail.com>
2016-03-26 15:12:09 +0200

    update

--- eMerge.js
+++ eMerge.js
@@ -8,6 +8,13 @@ var express = require("express");
 var app = express();
 var Firebase = require("firebase");
 var myFirebaseRef = new Firebase("https://torrid-fire-226.firebaseio.com");
+var serviceIndex;
+
+myFirebaseRef.child("servicesIndex").once('value', function(snapshot){
+   serviceIndex = snapshot.val()
+ });
+
+ console.console.log("ServiceIndex: " + ServiceIndex);
 
 //just an test call for debugging
   app.get("/test", function(req, res){
@@ -28,7 +35,9 @@ var myFirebaseRef = new Firebase("https://torrid-fire-226.firebaseio.com");
       "lname":lname,
       "town":town
     });
-    console.log("added new User...");
+    console.log('*************************');
+    console.log("*    added new User     *");
+    console.log('*************************');
   });
 
 //Adding a new emergency service
@@ -43,18 +52,18 @@ var myFirebaseRef = new Firebase("https://torrid-fire-226.firebaseio.com");
   var long = req.param("long");
   var usrRef = myFirebaseRef.child("services").child(serviceName);
 
-  usrRef.set(
-    {
-    "town":town,
-    "emphone":emphone,
-    "emphonealt":emphonealt,
-    "emtype":emtype,
-    "residence":residence,
-    "location":{
-      "lat":lat,
-      "long":long
-    },
-  });
+    usrRef.set(
+      {
+      "town":town,
+      "emphone":emphone,
+      "emphonealt":emphonealt,
+      "emtype":emtype,
+      "residence":residence,
+      "location":{
+        "lat":lat,
+        "long":long
+        },
+      });
   console.log('*************************');
   console.log("*   added new Service   *");
   console.log('*************************');
