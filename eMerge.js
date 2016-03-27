91f90673524904f70f35e0bb1edeb37961b1808d
bertie <coetzeealbertus@gmail.com>
2016-03-27 21:55:43 +0200

    update

--- eMerge.js
+++ eMerge.js
@@ -30,7 +30,7 @@ var polCoordinates = [];
 
     //var distFromCurrent = geolib.getDistance({latitude: usrLat, longitude: usrLong},{latitude: polCoordinates[0].lat, longitude: polCoordinates[0].long});
 
-    var nearestLoc = geolib.findNearest({latitude: usrLat, longitude: usrLong}, services, 1);
+    //var nearestLoc = geolib.findNearest({latitude: usrLat, longitude: usrLong}, services, 1);
     res.send("Distance from Current: "+services);
     });
   });
