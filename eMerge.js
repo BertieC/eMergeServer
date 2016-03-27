a250ec4e0a1b0e2053fa8fa5a7b3873a89522ee4
bertie <coetzeealbertus@gmail.com>
2016-03-27 17:19:43 +0200

    update

--- eMerge.js
+++ eMerge.js
@@ -14,7 +14,8 @@ var polCoordinates = [];
   app.get("/test", function(req, res){
   myFirebaseRef.child("services").child("police").child("0").once("value", function(snapshot) {
     var testVar = snapshot.val();
-    res.send(testVar.location);
+    polCoordinates.push(testVar.location));
+    res.send(polCoordinates[0]);
     });
   });
 
